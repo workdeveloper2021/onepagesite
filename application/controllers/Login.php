7333418856bb3c6e184dbc0999e63d5bd00f3255
praveen <work.developer20211@gmail.com>
2023-01-23 15:38:23 +0530

    'index'

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -15,7 +15,7 @@ class Login extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('Login_model','LM');
+        $this->load->model('Loginmodel','LM');
     }
 
     /**
--- application/models/Loginmodel.php
+++ application/models/Loginmodel.php
@@ -1,6 +1,6 @@
 <?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Login_model extends CI_Model
+class Loginmodel extends CI_Model
 {
     
     /**
